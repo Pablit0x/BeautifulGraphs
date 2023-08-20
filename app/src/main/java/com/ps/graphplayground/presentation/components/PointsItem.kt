3ea8fd164ed81a2dd1d605ebc6694dc652b7be86
noner <pawel495@interia.pl>
2023-08-20 22:53:56 +0100

    Design improvements...

--- app/src/main/java/com/ps/graphplayground/presentation/components/PointsItem.kt
+++ app/src/main/java/com/ps/graphplayground/presentation/components/PointsItem.kt
@@ -1,10 +1,17 @@
 package com.ps.graphplayground.presentation.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,8 +21,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import co.yml.charts.common.model.Point
 
 @Composable
@@ -27,36 +37,38 @@ fun PointsItem(
     var pointY by remember { mutableStateOf(point.y.toString()) }
 
     Row(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
         verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.spacedBy(4.dp)
+        horizontalArrangement = Arrangement.Center
     ) {
-        Text(text = "Point $index:", modifier = Modifier.weight(1f))
+        Text(text = "Point $index:", fontSize = 14.sp, fontWeight = FontWeight.Bold)
+
+        Spacer(modifier = Modifier.width(8.dp))
 
         OutlinedTextField(
             value = pointX, onValueChange = {
                 pointX = it
                 if (pointX.isNotEmpty()) {
-                    if (pointX.toFloat() != 0f) {
-                        xOnChange(pointX.toFloat())
-                    }
+                    xOnChange(pointX.toFloat())
                 }
             }, keyboardOptions = KeyboardOptions.Default.copy(
                 keyboardType = KeyboardType.Number
-            ), modifier = Modifier.weight(1f)
+            ), label = { Text(text = "x") }, modifier = Modifier.weight(1f)
         )
+
+        Spacer(modifier = Modifier.width(8.dp))
 
         OutlinedTextField(
             value = pointY, onValueChange = {
                 pointY = it
                 if (pointY.isNotEmpty()) {
-                    if (pointY.toFloat() != 0f) {
-                        yOnChange(pointY.toFloat())
-                    }
+                    yOnChange(pointY.toFloat())
                 }
             }, keyboardOptions = KeyboardOptions.Default.copy(
                 keyboardType = KeyboardType.Number
-            ), modifier = Modifier.weight(1f)
+            ), label = { Text(text = "y") }, modifier = Modifier.weight(1f)
         )
     }
 }
--- app/src/main/java/com/ps/graphplayground/presentation/graph_screen/GraphScreen.kt
+++ app/src/main/java/com/ps/graphplayground/presentation/graph_screen/GraphScreen.kt
@@ -1,23 +1,34 @@
 package com.ps.graphplayground.presentation.graph_screen
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import co.yml.charts.common.extensions.formatToSinglePrecision
 
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material.icons.filled.Remove
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalTextStyle
@@ -34,6 +45,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -51,12 +63,11 @@ import co.yml.charts.ui.linechart.model.SelectionHighlightPoint
 import co.yml.charts.ui.linechart.model.SelectionHighlightPopUp
 import co.yml.charts.ui.linechart.model.ShadowUnderLine
 import com.ps.graphplayground.presentation.components.PointsItem
-import java.text.DecimalFormat
 
 @Composable
 fun GraphScreen() {
-
-
+    var showPoints by remember { mutableStateOf(false) }
+    var showSettings by remember { mutableStateOf(false) }
     var steps by remember { mutableStateOf(5) }
     var dataPoints by remember {
         mutableStateOf(
@@ -65,14 +76,7 @@ fun GraphScreen() {
                 Point(x = 1f, y = 20f),
                 Point(x = 2f, y = 0f),
                 Point(x = 3f, y = 20f),
-                Point(x = 4f, y = 40f),
-                Point(x = 5f, y = 20f),
-                Point(x = 6f, y = 40f),
-                Point(x = 7f, y = 20f),
-                Point(x = 8f, y = 0f),
-                Point(x = 9f, y = 20f),
-                Point(x = 10f, y = 40f),
-                Point(x = 11f, y = 20f)
+                Point(x = 4f, y = 40f)
             )
         )
     }
@@ -80,10 +84,6 @@ fun GraphScreen() {
     var copy = dataPoints.toMutableList()
 
     var xAxisStepSize by remember { mutableStateOf(100.dp) }
-    var xAxisBackgroundColor by remember { mutableStateOf(Color.Transparent) }
-
-    var yAxisStepSize by remember { mutableStateOf(300.dp) }
-    var yAxisBackgroundColor by remember { mutableStateOf(Color.Transparent) }
 
     var xAxisLabelAndAxisLinePadding by remember { mutableStateOf(15.dp) }
     var yAxisLabelAndAxisLinePadding by remember { mutableStateOf(20.dp) }
@@ -96,23 +96,23 @@ fun GraphScreen() {
     var gridLines: GridLines? by remember { mutableStateOf(null) }
 
 
-    val xAxisData =
-        AxisData.Builder().axisStepSize(xAxisStepSize).backgroundColor(xAxisBackgroundColor)
-            .steps(dataPoints.size - 1).labelData { value -> value.toString() }
-            .labelAndAxisLinePadding(xAxisLabelAndAxisLinePadding)
-            .axisLineColor(MaterialTheme.colorScheme.primary)
-            .axisLabelColor(MaterialTheme.colorScheme.primary).axisLabelFontSize(xAxisLabelFontSize)
-            .build()
-
-    val yAxisData = AxisData.Builder().backgroundColor(yAxisBackgroundColor).steps(steps)
-        .labelAndAxisLinePadding(yAxisLabelAndAxisLinePadding).labelData { i ->
-            val yMin = dataPoints.minOf { it.y }
-            val yMax = dataPoints.maxOf { it.y }
-            val yScale = (yMax - yMin) / steps
-            ((i * yScale) + yMin).formatToSinglePrecision()
-        }.axisLineColor(MaterialTheme.colorScheme.primary)
-        .axisLabelColor(MaterialTheme.colorScheme.primary).axisLabelFontSize(yAxisLabelFontSize)
+    val xAxisData = AxisData.Builder().axisStepSize(xAxisStepSize).steps(dataPoints.size - 1)
+        .labelData { value -> value.toString() }
+        .labelAndAxisLinePadding(xAxisLabelAndAxisLinePadding)
+        .axisLineColor(MaterialTheme.colorScheme.primary)
+        .axisLabelColor(MaterialTheme.colorScheme.primary).axisLabelFontSize(xAxisLabelFontSize)
         .build()
+
+    val yAxisData =
+        AxisData.Builder().steps(steps).labelAndAxisLinePadding(yAxisLabelAndAxisLinePadding)
+            .labelData { i ->
+                val yMin = dataPoints.minOf { it.y }
+                val yMax = dataPoints.maxOf { it.y }
+                val yScale = (yMax - yMin) / steps
+                ((i * yScale) + yMin).formatToSinglePrecision()
+            }.axisLineColor(MaterialTheme.colorScheme.primary)
+            .axisLabelColor(MaterialTheme.colorScheme.primary).axisLabelFontSize(yAxisLabelFontSize)
+            .build()
 
     val lineChartData = LineChartData(
         linePlotData = LinePlotData(
@@ -142,9 +142,7 @@ fun GraphScreen() {
     )
 
     Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(8.dp)
+        modifier = Modifier.fillMaxSize().padding(16.dp), horizontalAlignment = Alignment.CenterHorizontally
     ) {
         LineChart(
             modifier = Modifier
@@ -152,63 +150,179 @@ fun GraphScreen() {
                 .height(200.dp), lineChartData = lineChartData
         )
 
-
-        LazyColumn(
-            horizontalAlignment = Alignment.CenterHorizontally
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
         ) {
+            ElevatedButton(
+                shape = RoundedCornerShape(40),
+                onClick = { showPoints = !showPoints },
+                modifier = Modifier.weight(2f)
+            ) {
+                Text(
+                    "Points",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 14.sp
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Icon(
+                    imageVector = if (showPoints) Icons.Default.KeyboardArrowUp else Icons.Default.KeyboardArrowDown,
+                    contentDescription = null
+                )
+            }
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Box(modifier = Modifier.weight(2f)) {
+                androidx.compose.animation.AnimatedVisibility(
+                    visible = showPoints, enter = fadeIn(), exit = fadeOut()
+                ) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Button(
+                            onClick = {
+                                copy.add(Point(x = copy.last().x + 1f, y = copy.last().y + 1f))
+                                dataPoints = copy
+                            }, colors = ButtonDefaults.buttonColors(
+                                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                                contentColor = MaterialTheme.colorScheme.primary
+                            )
+                        ) {
+                            Icon(imageVector = Icons.Default.Add, contentDescription = null)
+                        }
+
+                        Spacer(modifier = Modifier.width(8.dp))
+
+                        Button(
+                            onClick = {
+                                copy.removeLast()
+                                dataPoints = copy
+                            }, colors = ButtonDefaults.buttonColors(
+                                containerColor = MaterialTheme.colorScheme.errorContainer,
+                                contentColor = MaterialTheme.colorScheme.error
+                            )
+                        ) {
+                            Icon(imageVector = Icons.Default.Remove, contentDescription = null)
+                        }
+                    }
+                }
+            }
+        }
+
+        LazyColumn(modifier = Modifier.fillMaxWidth()
+        ) {
+
+
             itemsIndexed(dataPoints) { index, item ->
-                PointsItem(point = item, index = index, xOnChange = {
-                    copy[index] = Point(x = it, y = item.y)
-                    dataPoints = copy
-                }, yOnChange = {
-                    copy[index] = Point(x = item.x, y = it)
-                    dataPoints = copy
-                })
-            }
-            item {
-                OutlinedTextField(modifier = Modifier.padding(16.dp),
-                    value = "Steps $steps",
-                    onValueChange = {},
-                    enabled = false,
-                    textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.Center),
-                    trailingIcon = {
-                        IconButton(onClick = { steps++ }) {
-                            Icon(
-                                imageVector = Icons.Default.KeyboardArrowUp,
-                                contentDescription = null
-                            )
-                        }
-                    },
-                    leadingIcon = {
-                        IconButton(onClick = { steps-- }) {
-                            Icon(
-                                imageVector = Icons.Default.KeyboardArrowDown,
-                                contentDescription = null
-                            )
-                        }
+                AnimatedVisibility(
+                    visible = showPoints
+                ) {
+                    PointsItem(point = item, index = index, xOnChange = {
+                        copy[index] = Point(x = it, y = item.y)
+                        dataPoints = copy
+                    }, yOnChange = {
+                        copy[index] = Point(x = item.x, y = it)
+                        dataPoints = copy
                     })
-            }
-
-            item {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.spacedBy(4.dp)
-                ) {
-                    Text(text = "Dotted line")
-                    Switch(checked = isDotted, onCheckedChange = { isDotted = it })
                 }
             }
 
             item {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.spacedBy(4.dp)
-                ) {
-                    Text(text = "Show grid")
-                    Switch(checked = gridLines != null,
-                        onCheckedChange = { gridLines = if (it) GridLines(Color.Gray) else null })
+
+                ElevatedButton(onClick = { showSettings = !showSettings }) {
+                    Text("Show settings")
+                    Spacer(modifier = Modifier.width(4.dp))
+                    Icon(imageVector = Icons.Default.Settings, contentDescription = null)
+                }
+
+                AnimatedVisibility(visible = showSettings) {
+                    Column(
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Text(
+                                text = "Dotted line", fontWeight = FontWeight.Bold,
+                                fontSize = 16.sp,
+                            )
+                            Switch(checked = isDotted, onCheckedChange = { isDotted = it })
+                        }
+
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Text(
+                                text = "Show grid", fontWeight = FontWeight.Bold,
+                                fontSize = 16.sp,
+                            )
+                            Switch(checked = gridLines != null, onCheckedChange = {
+                                gridLines = if (it) GridLines(Color.Gray) else null
+                            })
+                        }
+
+                        OutlinedTextField(modifier = Modifier.fillMaxWidth(),
+                            value = "Steps $steps",
+                            onValueChange = {},
+                            enabled = false,
+                            textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.Center),
+                            trailingIcon = {
+                                IconButton(onClick = { steps++ }) {
+                                    Icon(
+                                        imageVector = Icons.Default.KeyboardArrowUp,
+                                        contentDescription = null,
+                                        tint = MaterialTheme.colorScheme.onSurface
+                                    )
+                                }
+                            },
+                            leadingIcon = {
+                                IconButton(onClick = { steps-- }) {
+                                    Icon(
+                                        imageVector = Icons.Default.KeyboardArrowDown,
+                                        contentDescription = null,
+                                        tint = MaterialTheme.colorScheme.onSurface
+                                    )
+                                }
+                            })
+                    }
                 }
             }
+//
+//            item {
+//                Row(
+//                    verticalAlignment = Alignment.CenterVertically,
+//                    horizontalArrangement = Arrangement.SpaceBetween,
+//                    modifier = Modifier.fillMaxWidth()
+//                ) {
+//                    Text(
+//                        text = "Dotted line", fontWeight = FontWeight.Bold,
+//                        fontSize = 14.sp,
+//                    )
+//                    Switch(checked = isDotted, onCheckedChange = { isDotted = it })
+//                }
+//            }
+//
+//            item {
+//                Row(
+//                    verticalAlignment = Alignment.CenterVertically,
+//                    horizontalArrangement = Arrangement.SpaceBetween,
+//                    modifier = Modifier.fillMaxWidth()
+//                ) {
+//                    Text(
+//                        text = "Show grid", fontWeight = FontWeight.Bold,
+//                        fontSize = 14.sp,
+//                    )
+//                    Switch(checked = gridLines != null,
+//                        onCheckedChange = { gridLines = if (it) GridLines(Color.Gray) else null })
+//                }
+//            }
         }
 
     }
